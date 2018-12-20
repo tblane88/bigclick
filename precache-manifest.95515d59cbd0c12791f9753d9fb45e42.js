041d0452aaa78277dd28d76b94ff2d64f9cbfc4a
tblane <tblane88@gmail.com>
2018-12-19 23:37:23 -0500

    Updates

---
+++ precache-manifest.95515d59cbd0c12791f9753d9fb45e42.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9fc33f3d068f2136a939",
+    "url": "/bigclick/static/js/runtime~main.9fc33f3d.js"
+  },
+  {
+    "revision": "a61e35daaf781762e489",
+    "url": "/bigclick/static/js/main.a61e35da.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/bigclick/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "a61e35daaf781762e489",
+    "url": "/bigclick/static/css/main.748b5e2e.chunk.css"
+  },
+  {
+    "revision": "bbbd8a0f05b2a7b09ac0c66378b78e70",
+    "url": "/bigclick/index.html"
+  }
+];
